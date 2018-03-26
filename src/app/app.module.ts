cc8d4600be1b38b165a4ea39d5d84842d5215f6a
Diego Warthon Hurtado <diegowarthon1190@gmail.com>
2018-03-25 19:01:11 -0500

    Quinta actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+//Servicios
+import { ListaDeseosService } from './services/lista-deseos.service';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -10,6 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { PendientesComponent } from '../pages/pendientes/pendientes.component';
 import { TerminadosComponent } from '../pages/terminados/terminados.component';
+
+  
 
 @NgModule({
   declarations: [
@@ -30,6 +35,7 @@ import { TerminadosComponent } from '../pages/terminados/terminados.component';
     PendientesComponent
   ],
   providers: [
+    ListaDeseosService,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
